90d10f772c865d5b397e29dce4b0f771ddb2d0b6
Ureen Jamil <165426961+Ureen-Jamil@users.noreply.github.com>
2024-04-07 19:44:22 +0500

    Update my first game tic tac toe.cpp

--- my first game tic tac toe.cpp
+++ my first game tic tac toe.cpp
@@ -123,7 +123,7 @@ bool gameover()
     {
         if (board[i][0] == board[i][1] && board[i][0] == board[i][2] || board[0][i] == board[1][i] && board[0][i] == board[2][i])
             return false;
-        if (board[0][0] == board[1][1] && board[0][0] == board[2][2] || board[0][2] == board[1][1] && board[0][0] == board[2][0])
+        if (board[0][0] == board[1][1] && board[0][0] == board[2][2] || board[0][2] == board[1][1] && board[0][2] == board[2][0])
             return false;
     }
 
